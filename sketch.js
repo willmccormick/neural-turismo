74878322e81511383793b70174407df7346550fc
RedH11 <44552711+RedH11@users.noreply.github.com>
2019-03-14 19:40:41 -0700

    Made the framework for the machine learning program
    I take a bunch of cars with randomly sorted seeds (1's and 0's where 1's move one speed(a variable) length up and 0's move one speed length down ) and then tested them to see whichever car had the best performance (not taking into account ties it just defaults to first car registered with the highest score)
    
    We need to figure out how to make this machine learning generational to start training an AI to actually make new generations of better cars (im pretty sure this is called generational learning)

--- sketch.js
+++ sketch.js
@@ -1,61 +1,87 @@
-var ship;
-var cowboys = [];
-var bullets = [];
+var direction = 90;
+var turnRadius = 0;
+var speed = 40; // Amount the car moves per click
+var seedsArray = [];
+var carsFitness = [];
+var carsArray = [];
+var topCarIndex = 0;
+var topFittness = 1000;
+var arrayIndex = 0;
+var randNum;
+var randString = "";
+var carAmount = 100;
 
 function setup() {
-  createCanvas(600, 400);
-  ship = new Ship();
-  //bullet = new Bullet(width/2, height/2);
-  for (var i = 0; i < 8; i++) {
-    cowboys[i] = new Cowboy(i * 80 + 80, 20);
+  createCanvas(1000, 800);
+  //creating 100 car sprites
+  for (var i = 0; i < carAmount; i++) {
+    carsArray[i] = createSprite(30 + (9*i), height / 2, 6, 12);
   }
+
+  generateSeeds();
+  testSeeds();
+  reviewSeeds();
 }
 
 function draw() {
-  background(51);
-  ship.show();
+  background(50, 50, 50);
+  fill(300);
+  ellipse(carsArray[topCarIndex].position.x,carsArray[topCarIndex].position.y, 55, 55);
+  /* // MANUAL COMMANDS
+  if (keyDown(40)) { // DOWN_ARROW
+    //car.setSpeed(speed, direction);
+    car.position.y += speed;
+  } else if (keyDown(38)) { // UP_ARROW
+    //car.setSpeed(speed, direction + 180)
+    car.position.y -= speed;
+  }*/
 
-  for (var i = 0; i < bullets.length; i++) {
-    bullets[i].show();
-    bullets[i].move();
+  drawSprites();
+}
 
-    for (var j = 0; j < cowboys.length; j++) {
-      if (bullets[i].hits(cowboys[j])) {
-        console.log("CONTACT");
-        cowboys[j].dissapear();
-        bullets[i].dissapear();
+function generateSeeds() {
+
+  // Generates the seeds needed for a first random generation
+  for (var cars = 0; cars < carAmount; cars++) {
+    for (var i = 0; i < 10; i++) {
+      randString += Math.round(Math.random());; // 0 or 1 random
+    }
+
+    seedsArray[cars] = randString;
+    randString = ""; // reset string after adding it
+  }
+}
+
+// Going to test all cars at one time
+function testSeeds() {
+
+  // For loop to get all of the cars
+  for (var c = 0; c < carAmount; c++) {
+    // Checking each part of the string (1 is up 0 is down)
+    for (var s = 0; s < 10; s++) {
+      if (seedsArray[c].charAt(s) === "1") {
+        carsArray[c].position.y -= speed; // if there is a 1 go up
+      } else {
+        carsArray[c].position.y += speed; // if there is a 0 go down
       }
     }
   }
 
-  for (var k = 0; k < cowboys.length; k++) {
-    cowboys[k].show();
-  }
-
-  for (var a = cowboys.length - 1; a >= 0; a--) {
-    if (cowboys[a].toDelete) {
-      cowboys.splice(a, 1);
-    }
-  }
-
-  for (var a = bullets.length - 1; a >= 0; a--) {
-    if (bullets[a].toDelete) {
-      bullets.splice(a, 1);
-    }
-  }
+  console.log("Generation Tested");
 }
 
-function keyPressed() {
+function reviewSeeds() {
 
-  if (key === ' ') {
-    var bullet = new Bullet(ship.x + 10, height);
-    bullets.push(bullet)
+  // Finds the car that traveled the farthest up
+  for (var c = 0; c < carAmount; c++) {
+    if (carsArray[c].position.y < topFittness) {
+      topCarIndex = c;
+      topFittness = carsArray[c].position.y;
+    } else {
+      console.log(carsArray[c].position.y + " is not less than " + topFittness);
+    }
   }
 
-  if (keyCode === RIGHT_ARROW) {
-    ship.move(1);
-  }
-  if (keyCode === LEFT_ARROW) {
-    ship.move(-1);
-  }
+  console.log("Fittest Car is number " + topCarIndex + "\nWith a distance of " + (carsArray[topCarIndex].position.y - height / 2) * -1);
+
 }
